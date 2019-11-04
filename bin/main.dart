02c2a21f2fd9ae38473b63b61ae8d329b47b3513
Sahil Singh <sahil.dogstar@gmail.com>
2019-11-04 16:32:37 +0530

    Finished day 22 morning challenge.

--- bin/main.dart
+++ bin/main.dart
@@ -26,6 +26,48 @@
 //    [9, 9, 3, 1],
 //  ]
 
+List<List<int>> mineSweeper(List<List<int>>input){
+  List<List<int>> result=[];
+  for(int i=0;i<input.length;i++){
+    List<int> resultRow=[];
+    for(int j=0;j<input[i].length;j++){
+      resultRow.add(calcNumber(input,i,j));
+    }
+    result.add(resultRow);
+  }
+  return result;
+}
+int calcNumber(List<List<int>> input, int row,int col){
+  if(input[row][col]==1){
+    return 9;
+  }
+  int count=0;
+  for(int offsetRow=-1;offsetRow<=1;offsetRow++){
+    for(int offsetCol=-1;offsetCol<=1;offsetCol++){
+      if(isBomb(input,row+offsetRow,col+offsetCol)){
+        count++;
+      }
+    }
+  }
+  return count;
+}
+bool isBomb(List<List<int>>input,int row, int col){
+  if(row<0){
+    return false;
+  }
+  if(col<0){
+    return false;
+  }
+  if(row>=input.length){
+    return false;
+  }
+  if(col>= input[row].length){
+    return false;
+  }
+  return input[row][col]==1;
+}
+
+
 // Challenge 3
 // Group in Order
 // Create a function that groups an array of numbers based on a size parameter. The size represents the maximum length of each sub-array.
@@ -42,6 +84,56 @@
 //  [[1, 4, 7, 10], [2, 5, 8, 11], [3, 6, 9]]
 /// "Leftover" arrays are okay.
 
+bool checkDivisor(int len, int num){
+  for(int j = num; 2 < j; j--){
+    if(len%j == 0){
+      return true;
+    }
+  }
+  return false;
+
+}
+
+List <List<int>> groupInOrder(List<int>list, int num) {
+  List <List<int>> finalList = [];
+
+
+  if(list.length%num == 0 || checkDivisor(list.length, num) == false) {
+    groupThis(list, num, finalList);
+  }
+
+  else if(list.length%num != 0 && checkDivisor(list.length, num) == true){
+    for(int k = num; 1 < k; k--){
+      if(list.length%k == 0){
+        groupThis(list, k, finalList);
+        break;
+      }
+    }
+  }
+
+  return finalList;
+}
+
+void groupThis(List<int> list, int num, List<List<int>> finalList) {
+  for (int i = 0; i < list.length; i += num) {
+    int end = (i + num < list.length) ? i + num : list.length;
+    finalList.add(list.sublist(i, end));
+  }
+}
+
 main() {
+
+  print(groupInOrder([1, 2, 3, 4, 5, 6], 3, ));
+  print(groupInOrder([1, 2, 3, 4, 5, 6], 2, ));
+  print(groupInOrder([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],4));
+  print(groupInOrder([2,4,6,8,10,12,14,16,18], 4));
+  print(groupInOrder([1,2,3,4,5,6,7,8,9,10,11], 4));
+  
+  print(mineSweeper([
+    [0, 1, 0, 0],
+    [0, 0, 1, 0],
+    [0, 1, 0, 1],
+    [1, 1, 0, 0],
+  ]));
 
 }
